5e6cd6e7b155fdd23511c80f3926b61d88e7bcf3
kelvi <kelvi_ugo@hotmail.com>
2022-04-17 10:59:43 -0300

    fix: jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,19 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts(x)'],
-    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
-};
+// jest.config.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>/', '<rootDir>/src/'],
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: ['**/*.{ts,tsx}']
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
