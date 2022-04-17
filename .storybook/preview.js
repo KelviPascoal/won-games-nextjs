e488fd6938c4e2d9f129e537324d1008319746d0
kelvi <kelvi_ugo@hotmail.com>
2022-04-17 11:03:07 -0300

    refactor: configura storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,19 @@
+import { GlobalStyles } from '../src/styles/global'
+
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+    />
+  ),
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +23,15 @@ export const parameters = {
     },
   },
 }
+
+import {theme} from '../src/styles/theme';
+import { ThemeProvider } from 'styled-components';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
