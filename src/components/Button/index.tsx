e3005bfd4445fc23bc2611f1f78a0b54bd638001
kelvi <kelvi_ugo@hotmail.com>
2022-04-19 09:29:07 -0300

    feat: finaliza componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import * as S from './styles';
 
 export type ButtonProps = {
@@ -5,15 +6,24 @@ export type ButtonProps = {
   size?: 'small' | 'medium' | 'large';
   fullWidth?: boolean;
   icon?: JSX.Element;
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 const Button = ({
   children,
   size = 'medium',
   fullWidth = false,
-  icon
+  icon,
+  onClick,
+  ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    onClick={onClick}
+    {...props}
+  >
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
