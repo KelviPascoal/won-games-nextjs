522ae43dbf65b039f62fdbbc0e197ed73dca2d93
kelvi <kelvi_ugo@hotmail.com>
2022-05-05 22:03:17 -0300

    feat: adiciona componente BannerSlider

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { BannerSlider, BannerSliderProps } from '.';
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  parameters: {
+    layout: 'fullscreen'
+  }
+};
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x581',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+];
+
+export const Exemple = (args: BannerSliderProps) => <BannerSlider {...args} />;
+
+Exemple.args = {
+  items: items
+};
---
+++ src/components/BannerSlider/test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import { BannerSlider } from '.';
+
+describe('<BannerSlider>', () => {
+
+    it('should be render a BannerSlider basic', () => {
+        render(<BannerSlider></BannerSlider>)
+    })
+})
